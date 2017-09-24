e77258e3a4859f24ca6386bfa3728ddd5890cfe4
Jordan Sleeper <survivingknowledge@gmail.com>
2017-09-23 21:24:38 -0400

    invalid without firstname

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 describe Contact do
-  it 'is valid with a firstname, lastname and email'
-  it 'is invalid without a firstname'
+  it 'is valid with a firstname, lastname and email' do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'tester@example.com'
+    )
+    expect(contact).to be_valid
+  end
+  it 'is invalid without a firstname' do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).not_to include("can't be blank")
+  end
   it 'is invalid without a lastname'
   it 'is invalid without a email address'
   it 'is invalid with a duplicate email adress'
