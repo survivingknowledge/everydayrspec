2d35ddd3acc1a2424cb8830c951959d0427f4045
Jordan Sleeper <survivingknowledge@gmail.com>
2017-10-07 18:44:54 -0400

    multiple phones through phone factories

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,33 +2,22 @@ require 'rails_helper'
 
 describe Phone do
   it 'does not allow duplicate phone numbers per contact' do
-    contact = Contact.create!(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'joetester@example.com'
-    )
-
-    contact.phones.create(
-      phone_type: 'home',
+    contact = FactoryGirl.create(:contact)
+    FactoryGirl.create(:home_phone,
+      contact: contact,
       phone: '785-555-1234'
     )
-    mobile_phone = contact.phones.build(
-      phone_type: 'mobile',
+    mobile_phone = FactoryGirl.build(:mobile_phone,
+      contact: contact,
       phone: '785-555-1234'
     )
 
     mobile_phone.valid?
-    expect(mobile_phone.errors[:phone]).to include('has already been taken')
+    expect(mobile_phone.errors[:phone]).to include ('has already been taken')
   end
 
   it 'allows two contacts to share a phone number' do
-    FactoryGirl.create(:phone,
-      phone_type: 'home',
-      phone: '785-555-1234'
-    )
-    expect(FactoryGirl.build(:phone,
-      phone_type: 'home',
-      phone: '785-555-1234'
-    )).to be_valid
+    FactoryGirl.create(:home_phone, phone: '785-555-1234')
+    expect(FactoryGirl.build(:home_phone, phone: '785-555-1234')).to be_valid
   end
 end
